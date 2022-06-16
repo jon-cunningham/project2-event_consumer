aee57b32a527a915abda0523554ecc32549a16a5
jon-cunningham <jon.cunningham@thebernardgroup.com>
2022-06-16 13:25:00 -0500

    Attempted to fix the handler function in app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from aws_lambda_powertools import Logger
 
+logger = Logger(service="JPC-Messaging-Service")
+
+@logger.inject_lambda_context(log_event=True)
 def handler(event, context):
     for record in event['records']:
         payload = record["body"]
-
-        # this should log the event
-        logger = Logger(service=payload)
+        logger.info(
+            {
+                "record": payload
+            }
+        )
